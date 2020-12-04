f21d281d7be905e271f9ca3f2e04e6bb60aaba63
Ilya Pirogov <ilja.pirogov@gmail.com>
2020-12-04 16:02:48 -0600

    Move out models from handlers

--- cmd/rosstat/internal/handlers/orders.go
+++ cmd/rosstat/internal/handlers/orders.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 
 	"github.com/labstack/echo/v4"
+
+	"rosstat/cmd/rosstat/internal/db"
 )
 
 type AllOrders struct {
@@ -45,29 +47,10 @@ func PutSmallToBuildOrders(c echo.Context) error {
 
 // GET /orders
 
-type SubOrderModel struct {
-	IsSmall       bool   `json:"is_small"`      // Тип подзаказа. true - small. false - big
-	OrderCaption  string `json:"order_caption"` // Заказ
-	AmountPallets int    `json:"amount_pallets"`
-	AmountBoxes   int    `json:"amount_boxes"`
-}
-
-type OrdersModel struct {
-	ID            int             `json:"id"`
-	Num           int             `json:"num"`           // Номер
-	OrderCaption  string          `json:"order_caption"` // Заказ
-	Customer      string          `json:"customer"`      // Заказчик
-	Address       string          `json:"address"`       // Адрес
-	Run           int             `json:"run"`           // Тираж
-	AmountPallets int             `json:"amount_pallets"`
-	AmountBoxes   int             `json:"amount_boxes"`
-	SubOrders     []SubOrderModel `json:"sub_orders"`
-}
-
 func GetToBuildOrders(c echo.Context) error {
 	ctx := c.(*RosContext)
 
-	result := []OrdersModel{
+	result := []db.OrdersModel{
 		{
 			ID:            1,
 			Num:           1,
@@ -77,7 +60,7 @@ func GetToBuildOrders(c echo.Context) error {
 			Run:           270,
 			AmountPallets: 1,
 			AmountBoxes:   1,
-			SubOrders: []SubOrderModel{
+			SubOrders: []db.SubOrderModel{
 				{
 					IsSmall:       false,
 					OrderCaption:  "О-20-123-РОССТАТ 2 (1-26)",
@@ -101,7 +84,7 @@ func GetToBuildOrders(c echo.Context) error {
 			Run:           1650,
 			AmountPallets: 0,
 			AmountBoxes:   0,
-			SubOrders: []SubOrderModel{
+			SubOrders: []db.SubOrderModel{
 				{
 					IsSmall:       false,
 					OrderCaption:  "О-22-355-РОССТАТ 1 (1-26)",
@@ -123,12 +106,6 @@ func GetToBuildOrders(c echo.Context) error {
 
 // GET /orders/big/build - used by /orders/big page
 
-type BigOrdersModel struct {
-	FormName string `json:"form_name"`
-	Total    int    `json:"total"`
-	Built    int    `json:"built"`
-}
-
 func GetBigToBuildOrders(c echo.Context) error {
 	ctx := c.(*RosContext)
 	orderID, err := strconv.Atoi(c.Param("id"))
@@ -140,7 +117,7 @@ func GetBigToBuildOrders(c echo.Context) error {
 	orderID = orderID
 
 	// get the data by orderID
-	result := []BigOrdersModel{
+	result := []db.BigOrdersModel{
 		{
 			FormName: "Форма №1. Записная книжечка переписчика (бла бла бла балб лабла бал)",
 			Total:    10,
